6757b6987a266ddb0cb1954c93bbb77b1c7afe1d
nahid-git <nahidcse.pub@gmail.com>
2024-01-16 02:44:12 +0600

    refactor toast

--- client/pages/login/index.js
+++ client/pages/login/index.js
@@ -1,148 +1,138 @@
-'use client'
-import React, { useEffect, useState } from 'react';
+"use client";
+import React, { useEffect, useState } from "react";
 import Link from "next/link";
-import Head from 'next/head'
-import { useForm } from "react-hook-form"
+import Head from "next/head";
+import { useForm } from "react-hook-form";
 import { signIn } from "next-auth/react";
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-import { useRouter } from 'next/router';
-import { useSearchParams } from 'next/navigation';
+import { toast } from "react-toastify";
+import { useRouter } from "next/router";
+import { useSearchParams } from "next/navigation";
 
 const Index = () => {
-    const router = useRouter()
-    const searchParams = useSearchParams()
-    const callbackUrl = searchParams.get('callbackUrl')
+  const router = useRouter();
+  const searchParams = useSearchParams();
+  const callbackUrl = searchParams.get("callbackUrl");
 
-    const {
-        register,
-        handleSubmit,
-        formState: { errors },
-        setError,
-    } = useForm({
-        mode: "onTouched"
-    })
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    setError,
+  } = useForm({
+    mode: "onTouched",
+  });
 
-    const loginForm = (loginData) => {
-        signIn("credentials", {
-            email: loginData?.email,
-            password: loginData?.password,
-            redirect: false
-        }).then((response) => {
-            if (response?.error) {
-                try {
-                    const errors = JSON.parse(response.error)
-                    errors.map((e) => {
-                        return setError(e.name, {
-                            type: 'manual',
-                            message: e.message[0],
-                        })
-                    })
-                } catch (error) {
-                    toast.error('Internal server error!', {
-                        position: "top-center",
-                        autoClose: 2000,
-                        hideProgressBar: false,
-                        closeOnClick: true,
-                        pauseOnHover: true,
-                        draggable: true,
-                        progress: undefined,
-                        theme: "colored",
-                    });
-                }
-            } else {
-                toast.success('Login Successful', {
-                    position: "top-center",
-                    autoClose: 2000,
-                    hideProgressBar: false,
-                    closeOnClick: true,
-                    pauseOnHover: true,
-                    draggable: true,
-                    progress: undefined,
-                    theme: "colored",
-                });
-                router.push(callbackUrl ?? '/profile')
-            }
-        })
-    }
+  const loginForm = (loginData) => {
+    signIn("credentials", {
+      email: loginData?.email,
+      password: loginData?.password,
+      redirect: false,
+    }).then((response) => {
+      if (response?.error) {
+        try {
+          const errors = JSON.parse(response.error);
+          errors.map((e) => {
+            return setError(e.name, {
+              type: "manual",
+              message: e.message[0],
+            });
+          });
+        } catch (error) {
+          toast.error("Internal server error!");
+        }
+      } else {
+        toast.success("Login Successful");
+        router.push(callbackUrl ?? "/profile");
+      }
+    });
+  };
 
-    return (
-        <>
-            <Head>
-                <title>Todo | Login</title>
-            </Head>
-            <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-[70vh] lg:py-0">
-                <div
-                    className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
-                    <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
-                        <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
-                            Sign in to your account
-                        </h1>
-                        <form className="space-y-4 md:space-y-6" onSubmit={handleSubmit(loginForm)}>
-                            <div>
-                                <label
-                                    htmlFor="email"
-                                    className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-                                >
-                                    Your email
-                                </label>
-                                <input
-                                    type="email"
-                                    id="email"
-                                    className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                                    placeholder="name@company.com"
-                                    {...register("email", {
-                                        required: 'Email is required',
-                                        pattern: {
-                                            value: /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/,
-                                            message: 'Invalid email address',
-                                        },
-                                    })}
-                                />
-                                <p className={'text-red-400 pl-1 pt-2 text-sm'}>{errors.email?.message}</p>
-                            </div>
-                            <div>
-                                <label
-                                    htmlFor="password"
-                                    className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-                                >
-                                    Password
-                                </label>
-                                <input
-                                    type="password"
-                                    id="password"
-                                    placeholder="••••••••"
-                                    className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                                    {...register("password", { required: 'Password is required' })}
-                                />
-                                <p className={'text-red-400 pl-1 pt-2 text-sm'}> {errors.password?.message} </p>
-                            </div>
-                            <div>
-                                <Link className="text-sm font-medium hover:underline" href="">
-                                    Forgot password?
-                                </Link>
-                            </div>
-                            <button type="submit"
-                                className="w-full text-white bg-indigo-500 hover:bg-indigo-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:focus:ring-primary-800"
-                            >
-                                Sign in
-                            </button>
-                            <p className="text-sm font-light text-gray-500 dark:text-gray-400">
-                                Don’t have an account yet?{" "}
-                                <Link
-                                    href="/signup"
-                                    className="font-medium text-primary-600 hover:underline dark:text-primary-500"
-                                >
-                                    Sign up
-                                </Link>
-                            </p>
-                        </form>
-                    </div>
-                </div>
-            </div>
-        </>
-    );
-
+  return (
+    <>
+      <Head>
+        <title>Todo | Login</title>
+      </Head>
+      <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-[70vh] lg:py-0">
+        <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+          <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+            <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+              Sign in to your account
+            </h1>
+            <form
+              className="space-y-4 md:space-y-6"
+              onSubmit={handleSubmit(loginForm)}
+            >
+              <div>
+                <label
+                  htmlFor="email"
+                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                >
+                  Your email
+                </label>
+                <input
+                  type="email"
+                  id="email"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  placeholder="name@company.com"
+                  {...register("email", {
+                    required: "Email is required",
+                    pattern: {
+                      value: /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/,
+                      message: "Invalid email address",
+                    },
+                  })}
+                />
+                <p className={"text-red-400 pl-1 pt-2 text-sm"}>
+                  {errors.email?.message}
+                </p>
+              </div>
+              <div>
+                <label
+                  htmlFor="password"
+                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                >
+                  Password
+                </label>
+                <input
+                  type="password"
+                  id="password"
+                  placeholder="••••••••"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  {...register("password", {
+                    required: "Password is required",
+                  })}
+                />
+                <p className={"text-red-400 pl-1 pt-2 text-sm"}>
+                  {" "}
+                  {errors.password?.message}{" "}
+                </p>
+              </div>
+              <div>
+                <Link className="text-sm font-medium hover:underline" href="">
+                  Forgot password?
+                </Link>
+              </div>
+              <button
+                type="submit"
+                className="w-full text-white bg-indigo-500 hover:bg-indigo-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:focus:ring-primary-800"
+              >
+                Sign in
+              </button>
+              <p className="text-sm font-light text-gray-500 dark:text-gray-400">
+                Don’t have an account yet?{" "}
+                <Link
+                  href="/signup"
+                  className="font-medium text-primary-600 hover:underline dark:text-primary-500"
+                >
+                  Sign up
+                </Link>
+              </p>
+            </form>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Index;
